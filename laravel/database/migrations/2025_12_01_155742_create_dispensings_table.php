6b81ce08167740ea6c9916136e1b6821bd6ef140
Mustain9 <mustainbellah@gmail.com>
2025-12-01 21:59:06 +0600

    dispensings table updated

--- laravel/database/migrations/2025_12_01_155742_create_dispensings_table.php
+++ laravel/database/migrations/2025_12_01_155742_create_dispensings_table.php
@@ -6,34 +6,28 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-         Schema::create('dispensings', function (Blueprint $table) {
-                    $table->id('DispensingID');
-                    $table->foreignId('PrescriptionID')->constrained('prescriptions', 'PrescriptionID');
-                    $table->foreignId('MedicineID')->constrained('medicines', 'MedicineID');
-                    $table->integer('QuantityDispensed');
-                    $table->decimal('UnitPrice', 10, 2);
-                    $table->decimal('TotalAmount', 10, 2);
-                    $table->foreignId('DispensedBy')->constrained('users', 'id');
-                    $table->timestamp('DispensedAt');
-                    $table->text('Notes')->nullable();
-                    $table->timestamps();
+        Schema::create('dispensings', function (Blueprint $table) {
+            $table->id('DispensingID');
+            $table->foreignId('PrescriptionID')->constrained('prescriptions', 'PrescriptionID');
+            $table->foreignId('MedicineID')->constrained('medicines', 'MedicineID');
+            $table->integer('QuantityDispensed');
+            $table->decimal('UnitPrice', 10, 2);
+            $table->decimal('TotalAmount', 10, 2);
+            $table->foreignId('DispensedBy')->constrained('users', 'id');
+            $table->timestamp('DispensedAt');
+            $table->text('Notes')->nullable();
+            $table->timestamps();
 
-                    $table->index('PrescriptionID');
-                    $table->index('MedicineID');
-                    $table->index('DispensedBy');
-                    $table->index('DispensedAt');
+            $table->index('PrescriptionID');
+            $table->index('MedicineID');
+            $table->index('DispensedBy');
+            $table->index('DispensedAt');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('dispensings');
     }
