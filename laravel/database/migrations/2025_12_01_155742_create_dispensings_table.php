a64ca7076cfa3761c9c50e837311fe446a332b5c
Mustain9 <mustainbellah@gmail.com>
2025-12-01 21:58:50 +0600

    dispensings table update

--- laravel/database/migrations/2025_12_01_155742_create_dispensings_table.php
+++ laravel/database/migrations/2025_12_01_155742_create_dispensings_table.php
@@ -11,9 +11,22 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('dispensings', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+         Schema::create('dispensings', function (Blueprint $table) {
+                    $table->id('DispensingID');
+                    $table->foreignId('PrescriptionID')->constrained('prescriptions', 'PrescriptionID');
+                    $table->foreignId('MedicineID')->constrained('medicines', 'MedicineID');
+                    $table->integer('QuantityDispensed');
+                    $table->decimal('UnitPrice', 10, 2);
+                    $table->decimal('TotalAmount', 10, 2);
+                    $table->foreignId('DispensedBy')->constrained('users', 'id');
+                    $table->timestamp('DispensedAt');
+                    $table->text('Notes')->nullable();
+                    $table->timestamps();
+
+                    $table->index('PrescriptionID');
+                    $table->index('MedicineID');
+                    $table->index('DispensedBy');
+                    $table->index('DispensedAt');
         });
     }
 
