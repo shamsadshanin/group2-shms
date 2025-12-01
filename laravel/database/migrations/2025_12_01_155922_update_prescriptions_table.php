90b8fcb39f8cb67dfb0aedd78eaeff060f14bcf1
Mustain9 <mustainbellah@gmail.com>
2025-12-01 22:05:04 +0600

    prescriptions_table update

--- laravel/database/migrations/2025_12_01_155922_update_prescriptions_table.php
+++ laravel/database/migrations/2025_12_01_155922_update_prescriptions_table.php
@@ -11,7 +11,18 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+      Schema::table('prescriptions', function (Blueprint $table) {
+                  $table->enum('Status', ['Pending', 'Dispensed', 'Partially_Dispensed', 'Cancelled'])->default('Pending');
+                  $table->foreignId('MedicineID')->nullable()->constrained('medicines', 'MedicineID');
+                  $table->integer('QuantityPrescribed');
+                  $table->integer('QuantityDispensed')->default(0);
+                  $table->timestamp('DispensedAt')->nullable();
+                  $table->foreignId('DispensedBy')->nullable()->constrained('users', 'id');
+                  $table->text('PharmacyNotes')->nullable();
+
+                  $table->index('Status');
+                  $table->index('MedicineID');
+                  $table->index('DispensedAt');
     }
 
     /**
